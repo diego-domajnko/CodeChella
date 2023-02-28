097b5ec59e120c7620ac693420fc092d599097ae
diego-domajnko <diegodomajnko@gmail.com>
2023-02-28 01:10:29 -0300

    criação da pagina de informações

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,18 @@ menu.addEventListener('click', () => {
     listaMenu.classList.remove('nav__lista--ativo');
   }
 })
+
+
+const perguntas = document.querySelectorAll('.pergunta__card__titulo');
+
+perguntas.forEach(element => {
+  element.addEventListener('click', () => {
+    let resposta = element.parentNode.querySelector('.pergunta__card__resposta').classList;
+
+    if (!resposta.contains('hidden')) {
+      resposta.add('hidden');
+    } else {
+      resposta.remove('hidden');
+    }
+  })
+});

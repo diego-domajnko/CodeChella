1d9c43766ae5db75bc23aaf8973b29b78c06daab
diego-domajnko <diegodomajnko@gmail.com>
2023-03-06 10:28:31 -0300

    update js

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,7 @@ const menu = document.querySelector('.nav__icone-menu');
 const listaMenu = document.querySelector('.nav__lista');
 
 menu.addEventListener('click', () => {
-  if (!listaMenu.classList.contains('nav__lista--ativo')) {
-    listaMenu.classList.add('nav__lista--ativo');
-  } else {
-    listaMenu.classList.remove('nav__lista--ativo');
-  }
+  listaMenu.classList.toggle('nav__lista--ativo');
 });
 
 
@@ -14,12 +10,7 @@ const perguntas = document.querySelectorAll('.pergunta__card__titulo');
 
 perguntas.forEach(element => {
   element.addEventListener('click', () => {
-    let resposta = element.parentNode.querySelector('.pergunta__card__resposta').classList;
-
-    if (!resposta.contains('hidden')) {
-      resposta.add('hidden');
-    } else {
-      resposta.remove('hidden');
-    }
+    const resposta = element.parentNode.querySelector('.pergunta__card__resposta').classList;
+    resposta.toggle('hidden')
   })
 });

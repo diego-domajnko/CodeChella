f8e87b79d7243b5eb5d02c7c1f55e591b0e6cf19
diego-domajnko <diegodomajnko@gmail.com>
2023-03-13 18:43:25 -0300

    updates

--- assets/js/compraIngressos.js
+++ assets/js/compraIngressos.js
@@ -3,28 +3,44 @@ const form = document.querySelector('.ingresso__form');
 form.addEventListener('submit', (e) => {
   e.preventDefault();
 
-  const pessoa = {
-    nome: '',
-    ingresso: ''
-  }
+  const pessoa = {}
+  const caixasSelecao = document.querySelectorAll('select');
 
   pessoa.nome = document.getElementById('nome').value;
-  pessoa.ingresso = selecionarTipoIngresso();
+  caixasSelecao.forEach(element => {
+    if (element.id == 'setor') {
+      pessoa.setor = selecionarIngressoSetor(element.id);
+    } else {
+      pessoa.ingresso = selecionarIngressoSetor(element.id);
+    }
+  });
 
   localStorage.setItem('comprador', JSON.stringify(pessoa));
 
   window.location.href = "../pages/ingresso-comprado.html";
 })
 
-function selecionarTipoIngresso() {
-  const ingresso = document.querySelector('#ingresso').querySelectorAll('option');
-  let tipoIngresso = '';
+function selecionarIngressoSetor(id) {
+  const lista = document.querySelector(`#${id}`).querySelectorAll('option');
+  let opcaoSelecionada = '';
 
-  ingresso.forEach(element => {
+  lista.forEach(element => {
     if (element.selected) {
-      tipoIngresso = element.textContent;
+      opcaoSelecionada = element.textContent;
     }
   });
 
-  return tipoIngresso;
+  return opcaoSelecionada;
 }
+
+// function validarIdade() {
+//   const dataNascimento = new Date(document.querySelector('#nascimento').value)
+//   const dataMaiorIdade = new Date(dataNascimento.getUTCFullYear() + 16, dataNascimento.getMonth(), dataNascimento.getDate())
+//   const hoje = new Date();
+
+//   return hoje >= dataMaiorIdade;
+// }
+
+// function validarCPF() {
+
+// }
--- assets/js/exibirIngresso.js
+++ assets/js/exibirIngresso.js
@@ -5,7 +5,7 @@ function exibirIngresso() {
   dadosCompra.innerHTML = `
     <h3 class="h6 nome-comprador">${pessoa.nome}</h3>
     <span class="p tipo-ingresso">${pessoa.ingresso}</span>
-    <span class="p setor">Setor Pista</span>
+    <span class="p setor">${pessoa.setor}</span>
     <span class="p data-show">Data: 11/03</span>
     <span class="p local-show">Local: São Paulo - SP</span>
   `

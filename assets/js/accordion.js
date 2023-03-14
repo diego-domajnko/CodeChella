2c4ebbb1441259629c31d196cd952cee25794b46
diego-domajnko <diegodomajnko@gmail.com>
2023-03-14 15:27:26 -0300

    update accordion

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -2,9 +2,18 @@ const perguntas = document.querySelectorAll('.pergunta__card__titulo');
 
 perguntas.forEach(element => {
   element.addEventListener('click', () => {
+    esconderRespostas();
     accordion(element.parentNode);
   })
 });
+
+function esconderRespostas() {
+  perguntas.forEach(e => {
+    e.parentNode.querySelector('.pergunta__card__resposta').classList.add('hidden');
+    e.parentNode.querySelector('.fa-caret-up').classList.add('hidden');
+    e.parentNode.querySelector('.fa-caret-down').classList.remove('hidden');
+  })
+}
 
 function accordion(elemento) {
   const resposta = elemento.querySelector('.pergunta__card__resposta').classList;
